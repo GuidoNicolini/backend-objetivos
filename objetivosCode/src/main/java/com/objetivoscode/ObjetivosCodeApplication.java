fa892e8cb2139cb5a17b3c09ca95eb579ae052a5
GuidoNicolini <91362886+GuidoNicolini@users.noreply.github.com>
2022-11-26 19:08:21 -0300

    creacion

---
+++ objetivosCode/src/main/java/com/objetivoscode/ObjetivosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.objetivoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivosCodeApplication.class, args);
+	}
+
+}
---
+++ objetivosCode/src/test/java/com/objetivoscode/ObjetivosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objetivoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivosCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
